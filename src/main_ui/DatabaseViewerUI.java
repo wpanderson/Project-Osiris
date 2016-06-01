e7d3caf1ab55b83d15993b1ed52748f93a1e29c3
Weston Anderson <weston.anderson.p@gmail.com>
2016-06-01 16:58:30 -0700

    commit to sync

--- src/main_ui/DatabaseViewerUI.java
+++ src/main_ui/DatabaseViewerUI.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package main_ui;
+
 
 import D5DataStructures.CSVIO;
 import java.io.BufferedReader;
