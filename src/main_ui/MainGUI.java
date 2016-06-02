c55ca354f8bcc37540d1703bb59a2a1c662dec80
patel7 <supatel@live.com>
2016-06-01 18:47:17 -0700

    Stuff

--- src/main_ui/MainGUI.java
+++ src/main_ui/MainGUI.java
@@ -52,7 +52,7 @@ public class MainGUI extends javax.swing.JFrame {
      */
     private static void LoadDatabase()
     {
-        
+        /*
         //CSVIO readIn = new CSVIO();
         try {
             //change this at a later time to get the path at runtime
@@ -66,10 +66,10 @@ public class MainGUI extends javax.swing.JFrame {
         }
         
         System.out.println(DATABASE.getEnemyList());
-        
-        // reads in from 
-        /* Serializer.loadFromFile("testfile.dragons");
-        System.out.println(MainGUI.DATABASE.getEnemyList()); */
+        */
+        // reads in from serlialized default file
+        Serializer.loadFromFile("testfile.dragons");
+        System.out.println(MainGUI.DATABASE.getEnemyList());
     }
     /**
      * Displays content that is in the database to all the pages.
