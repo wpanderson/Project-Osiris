2fdec85b66ae6194561771ef917307bdd34d57a3
patel7 <supatel@live.com>
2016-06-01 18:18:02 -0700

    More checks of serialization
    not important to keep these, just checking if stuff works

--- src/main_ui/MainGUI.java
+++ src/main_ui/MainGUI.java
@@ -52,7 +52,7 @@ public class MainGUI extends javax.swing.JFrame {
      */
     private static void LoadDatabase()
     {
-        /*
+        
         //CSVIO readIn = new CSVIO();
         try {
             //change this at a later time to get the path at runtime
@@ -66,9 +66,10 @@ public class MainGUI extends javax.swing.JFrame {
         }
         
         System.out.println(DATABASE.getEnemyList());
-        */
-        Serializer.loadFromFile("testfile.dragons");
-        System.out.println(MainGUI.DATABASE.getEnemyList());
+        
+        // reads in from 
+        /* Serializer.loadFromFile("testfile.dragons");
+        System.out.println(MainGUI.DATABASE.getEnemyList()); */
     }
     /**
      * Displays content that is in the database to all the pages.
